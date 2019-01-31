837ffe317444305e6960afad91d0a427e703a6df
yuna <yoona3316@naver.com>
2019-01-31 21:27:18 +0900

    Fanmeeting version2 - still timeover

--- APSS/src/chapter7/Fanmeeting.java
+++ APSS/src/chapter7/Fanmeeting.java
@@ -3,48 +3,59 @@ package chapter7;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Fanmeeting {
     static int testcase;
     static int idolNum;
     static int fanNum;
-    static char[] idols;
-    static char[] fans;
-    static int res=0;
+    static Stack<Integer> idols;
+//    static Stack<Integer> fans;
+    static String fans;
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         testcase = Integer.parseInt(br.readLine());
         for (int test=0; test<testcase; test++) {
+            idols=new Stack<Integer>();
             String str = br.readLine();
             idolNum = str.length();
-            idols = new char[idolNum];
             for (int i=0; i<idolNum; i++) {
-                idols[i] = str.charAt(i);
+                if (str.charAt(i)=='M') idols.push(i);
             }
-            str = br.readLine();
-            fanNum = str.length();
-            fans = new char[fanNum];
-            for (int i=0; i<fanNum; i++) {
-                fans[i] = str.charAt(i);
-            }
-            solve();
-            System.out.println(res);
-            res = 0;
+            fans = br.readLine();
+            fanNum = fans.length();
+            System.out.println(fanNum-idolNum+1-solve2());
         }
 
     }
-    public static void solve() {
-        int offset = 0;
+    public static boolean cantHug(int offset) {
+        for (int i=0; i<idols.size(); i++) {
+            if (fans.charAt(offset+idols.get(i))=='M') return true;
+        }
+        return false;
+    }
+    public static int solve2() {
+        if (idols.size()==0) return 0;
+        int offset=0;
+        int canthug=0;
         while (offset<=(fanNum-idolNum)) {
-            boolean allHug=true;
-            for (int i=0; i<idolNum; i++) {
-                if (idols[i]=='M'&&fans[i+offset]=='M') {
-                    allHug = false;
-                    break;
-                }
-            }
-            if (allHug) res++;
+            if (cantHug(offset)) canthug++;
             offset++;
         }
+        return canthug;
     }
+//    public static void solve() {
+//        int offset = 0;
+//        while (offset<=(fanNum-idolNum)) {
+//            boolean allHug=true;
+//            for (int i=0; i<idolNum; i++) {
+//                if (idols[i]=='M'&&fans[i+offset]=='M') {
+//                    allHug = false;
+//                    break;
+//                }
+//            }
+//            if (allHug) res++;
+//            offset++;
+//        }
+//    }
 }
